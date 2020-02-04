8088cf8efd9bfc6adeaf0b3407c7e08fa9c9237f
giorgiagandolfi <59025266+giorgiagandolfi@users.noreply.github.com>
2020-02-04 15:20:28 +0100

    update S-W

--- exercises/S-W_alg.py
+++ exercises/S-W_alg.py
@@ -16,10 +16,13 @@ def score(s1,s2,S,d,t):
     trace_back=[]
     m=len(s1)+1
     n=len(s2)+1
+    #initialize the two matrices as zero matrices
     F=[[0]*m for x in range(n)]
     trace_back=[[0]*m for x in range(n)]
+    #edit first row of trace back matrix 
     for i in range(len(F[0][:-1])):
         trace_back[0][i+1]="L"
+    #edit first column of trace back matrix
     for i in range(len(F[:-1])):
         trace_back[i+1][0]="U"
     for i in range(1,n):#iteration based on the maximization of the value
@@ -27,7 +30,10 @@ def score(s1,s2,S,d,t):
             diag=F[i-1][j-1]+S[s1[j-1]+s2[i-1]]#match
             left=F[i][j-1]+d #gap in the second sequence
             up=F[i-1][j]+d#gap in the first sequence
+            #fill up the matrix according to the highest value between zero, diag, left and up
+            #negative values are avoided
             F[i][j]=max(diag,left,up,0)
+            #fill up trace back matrix
             if F[i][j]==diag:
                 trace_back[i][j]="D"
             elif F[i][j]==left:
@@ -35,7 +41,10 @@ def score(s1,s2,S,d,t):
             elif F[i][j]==up:
                 trace_back[i][j]="U"
     #the starting point for the trace back is the highest value in the matrix of scores
-    #in order to find the indices of the highest value i 
+    #put all values of the F matrix into a list
+    #assign to a variable the highest value
+    #find the value into the row of F matrix (F[i]) in order to retrive i index (the position in the row)
+    #find the value with its i index into F columns (F[i_max][j]) and retrive j index (the position in the column)
     values=[]
     for i in range(n):
         for j in range(m):
@@ -51,6 +60,7 @@ def score(s1,s2,S,d,t):
     al2=""
     j=j_max
     i=i_max
+    #trace back until a value equal to zero is found
     while F[i][j]!=0:
         if trace_back[i][j]=="D":
             al1+=s1[j-1]
